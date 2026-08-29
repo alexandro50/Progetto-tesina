bdd25b4a08a844e5ec7a47dfe9fd9cf6a51d9ebd
alexandro50 <Aldoalexandromalgarejomachare@itstechtalentfactory.it>
2026-08-29 11:30:38 +0200

    aggiunta di backend

---
+++ WorkShift Manager/backend/backend_utenti/src/main/java/com/example/spring/backend_utenti/BackendUtentiApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring.backend_utenti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendUtentiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendUtentiApplication.class, args);
+	}
+
+}
---
+++ WorkShift Manager/backend/backend_utenti/src/test/java/com/example/spring/backend_utenti/BackendUtentiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring.backend_utenti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendUtentiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
