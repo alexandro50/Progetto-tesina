8015af14770323a8252b021a94472efd0f162ade
alexandro50 <Aldoalexandromalgarejomachare@itstechtalentfactory.it>
2026-09-02 12:44:31 +0200

    nuovo

--- WorkShift Manager/backend/backend_utenti/src/main/java/com/example/spring/backend_utenti/security/UserDetailsServiceImpl.java
+++ WorkShift Manager/backend/backend_utenti/src/main/java/com/example/spring/backend_utenti/security/UserDetailsServiceImpl.java
@@ -1,8 +1,5 @@
 package com.example.spring.backend_utenti.security;
 
-import java.util.Collection;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
